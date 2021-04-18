c4ea13d5e7005a65963c911c9e2df451e00d3d45
sabrina <sabrina19960226@gmail.com>
2021-04-18 16:58:43 +0800

    Update 2.clock

--- 2. clock/main.js
+++ 2. clock/main.js
@@ -1,9 +1,16 @@
-// 資料處理  抓時間 時 分 秒
-
-let hours = document.querySelector('.hours').
-hours = new Date()
-console.log(hours)
-// const mins = 
-// const seds =
+// 資料處理 
 
 
+//取得DOM
+const hour_hand = document.querySelector('.hour');
+const min_hand = document.querySelector('.min');
+const sec_hand = document.querySelector('.sec');
+
+
+//抓住data 時間
+const now = new Date()
+const hour = now.getHours();
+const min = now.getMinutes();
+const sec = now.getSeconds();
+
+console.log(now,hour,min,sec)
