dccd215007f518cd41adda0896df2b7628cadd78
sabrina <sabrina19960226@gmail.com>
2021-04-18 23:49:47 +0800

    Update 2.clock css & js

--- 2. clock/main.js
+++ 2. clock/main.js
@@ -6,11 +6,24 @@ const hour_hand = document.querySelector('.hour');
 const min_hand = document.querySelector('.min');
 const sec_hand = document.querySelector('.sec');
 
+function setData(){
+    //抓住date 時間
+    const now = new Date()
+    const hour = now.getHours();
+    const min = now.getMinutes();
+    const sec = now.getSeconds();
 
-//抓住data 時間
-const now = new Date()
-const hour = now.getHours();
-const min = now.getMinutes();
-const sec = now.getSeconds();
+    //計算旋轉角度
+    //每一時,分,秒鐘，指針需要旋轉幾度
+    const hourDegress  = ((hour / 12) * 360)
+    const minDegress  = ((min / 60) * 360)
+    const secDegress = ((sec / 60) * 360)
+    //寫入style
+    hour_hand.style.transform = `rotate(${hourDegress}deg)` 
+    min_hand.style.transform = `rotate(${minDegress}deg)`   
+    sec_hand.style.transform = `rotate(${secDegress}deg)`  
+}
 
-console.log(now,hour,min,sec)
+
+// 每1秒（1000 毫)
+setInterval(setData, 1000);
