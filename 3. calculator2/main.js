ba5988399a939a02c03b438c2bd2c097137b6bae
sabrina <sabrina19960226@gmail.com>
2021-04-20 21:22:38 +0800

    Update 3. calculator2 js

---
+++ 3. calculator2/main.js
@@ -0,0 +1,14 @@
+
+let decimalbtn = document.getElementById('calc-decimal')//點數
+let clearbtn = document.getElementById('calc-clear')//全部清除
+let backspacebtn = document.getElementById('calc-backspace')//刪除
+let displayValElement = document.getElementById('calc-display-val')//運算結果
+
+let calcnumbtn = document.getElementsByClassName('calc-btn-num ')//所有數字
+let calcoperatorbtn = document.getElementsByClassName('calc-btn-operator')//加減乘除
+
+let displayVal = '0'; //初始值
+let pendingVal; //追蹤被影藏的數字 ex. 5+3時顯示3而5被隱藏
+let evalStringArray = [];
+
+//////////////////////////////////
