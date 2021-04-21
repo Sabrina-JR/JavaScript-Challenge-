1f265c9c9b82e9bbb920a1aa215399df331d997d
Sabrina <sabrina19960226@gmail.com>
2021-04-21 15:23:28 +0800

    Revise decimal button

--- 3. calculator/main.js
+++ 3. calculator/main.js
@@ -3,7 +3,7 @@ let decimalbtn = document.querySelector('.btn-decimal');//點數
 let clearbtn = document.querySelector('.btn-clear')//全部清除
 let backspacebtn = document.querySelector('.btn-backspace')//刪除
 let displayResult = document.querySelector('.displayResult')//運算結果
-let displayDetail = document.querySelectorAll('.displayDetail')//運算過程
+let displayDetail = document.querySelector('.displayDetail')//運算過程
 
 
 let calcnumbtn = document.querySelectorAll('.btn-num') //所有數字
@@ -23,7 +23,6 @@ let updateDisplayResult = (e) =>{
 
     displayVal+= btnText;
     displayResult.innerText = displayVal;
-    console.log(displayVal)
 }
 
 // for迴圈抓住所有btn-num監聽click事件
@@ -49,3 +48,11 @@ backspacebtn.addEventListener('click',()=>{
     displayResult.innerText = displayVal;
 })
 
+//點數
+
+decimalbtn.addEventListener('click',()=>{
+    if(!displayVal.includes('.'))
+        displayVal+='.'
+    
+    displayResult.innerText = displayVal;
+})
