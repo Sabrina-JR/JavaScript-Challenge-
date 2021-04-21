7197e7135047554a7b8729842e5dd0b71a7e4d6c
Sabrina <sabrina19960226@gmail.com>
2021-04-21 13:09:17 +0800

    Update 3. calculator

---
+++ 3. calculator/main.js
@@ -0,0 +1,10 @@
+//取DOm
+let decimalbtn = document.querySelector('.btn-decimal');//點數
+let clearbtn = document.querySelector('.btn-clear')//全部清除
+let backspacebtn = document.querySelector('.btn-backspace')//刪除
+let displayResult = document.querySelector('.displayResult')//運算結果
+
+
+let calcnumbtn = document.querySelectorAll('.btn-num') //所有數字
+let calcoperatorbtn = document.querySelectorAll('.btn-operator')//加減乘除 
+console.log(decimalbtn,clearbtn,backspacebtn,displayResult,calcnumbtn,calcoperatorbtn)
